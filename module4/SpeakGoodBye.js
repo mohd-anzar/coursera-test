ee3de55285e30bbfb19a8c174ef6a3953a9d16c5
mohd anzar <mohd.anzar345@gmail.com>
2020-09-13 13:42:28 +0530

    my thirs page

--- module4/SpeakGoodBye.js
+++ module4/SpeakGoodBye.js
@@ -17,7 +17,7 @@ var seakWord = "Good Bye";
 // byeSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
 function seak(name) {
-  console.log(seakWord + " J" + name);
+  console.log(seakWord + " " + name);
 }
 
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
